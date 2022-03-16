c915db48d731924c7ab14a6cb34022b7cd4b854c
kuy <kuy472013@gmail.com>
2022-03-16 23:33:02 +0900

    Set Spider Arguments

--- scraper/spiders/musinsa.py
+++ scraper/spiders/musinsa.py
@@ -4,7 +4,19 @@ import scrapy
 class MusinsaSpider(scrapy.Spider):
     name = 'musinsa'
     allowed_domains = ['www.musinsa.com']
-    start_urls = ['http://www.musinsa.com/']
+    start_urls = [
+        'https://www.musinsa.com/ranking/brand',
+        'https://www.musinsa.com/ranking/best',
+        'https://www.musinsa.com/ranking/keyword',
+    ]
+
+    def __init__(self, category=None):
+        self.category = category
+
+    def start_requests(self):
+        for url in self.start_urls:
+            print(url)
+            yield scrapy.Request(url + f'/mainCategory={self.category}', callback=self.parse)
 
     def parse(self, response):
-        pass
+        self.logger.info('test')
